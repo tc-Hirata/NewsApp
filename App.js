c31ad58ed9b66a5a32bac32583bc3718a8ebae9e
tc-hirata <hirata.naoko.timecapsule@gmail.com>
2023-02-14 12:39:30 +0900

    コンポーネントの作成、ダミーデータの追加

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView, FlatList} from "react-native";
+import { ListItem } from "./components/ListItem";
+import articles from "./dummies/articles"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem = {({ item }) => {
+          return (
+            <ListItem
+            imageUrl = {item.urlToImage}
+            title = {item.title}
+            author = {item.author}
+            />
+          )
+        }}
+        keyExtractor = {(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#eeeeee",
   },
 });
