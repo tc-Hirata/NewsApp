2318404783778fbfcf2e19abf1d737a1769adc84
tc-hirata <hirata.naoko.timecapsule@gmail.com>
2023-02-14 14:20:41 +0900

    expo-constantsの追加、Apiを追加してニュース記事を読み込む

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, FlatList} from "react-native";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { ListItem } from "./components/ListItem";
-import articles from "./dummies/articles"
+import axios from "axios";
+import Constants from "expo-constants";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={articles}
-        renderItem = {({ item }) => {
-          return (
-            <ListItem
-            imageUrl = {item.urlToImage}
-            title = {item.title}
-            author = {item.author}
-            />
-          )
+        renderItem={({ item }) => {
+          return <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />;
         }}
-        keyExtractor = {(item, index) => index.toString()}
+        keyExtractor={(item, index) => index.toString()}
       />
       <StatusBar style="auto" />
     </SafeAreaView>
